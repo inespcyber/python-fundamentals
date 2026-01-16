9c03d3ab00d83a5079601508e88d8f0875370109
Inês Pereira <ines22p@gmail.com>
2026-01-16 13:12:58 +0000

    Update age_verification.py

--- conditionals/age_verification.py
+++ conditionals/age_verification.py
@@ -16,6 +16,7 @@ This script is created for educational purposes to practise Python fundamentals.
 """
 # Ask the user to enter their age
 age = int(input("Enter your age: "))
+
 # Check if the user is an adult or a minor and print the result
 if age >= 18:
     print("You are of legal age.")
